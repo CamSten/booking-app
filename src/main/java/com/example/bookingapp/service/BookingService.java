4da588706281d0c77593ea6e43a5688c0eb98a51
CamSten <camilla.stenklov@gmail.com>
2026-05-08 14:46:02 +0200

    Adding fields to entity Booking, adding methods to BookingService and BookingController, for database requests.

--- src/main/java/com/example/bookingapp/service/BookingService.java
+++ src/main/java/com/example/bookingapp/service/BookingService.java
@@ -2,19 +2,49 @@ package com.example.bookingapp.service;
 
 import com.example.bookingapp.model.Booking;
 import com.example.bookingapp.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookingService {
+    @Autowired
     private final BookingRepository bookingRepository;
 
     public BookingService(BookingRepository bookingRepository) {
         this.bookingRepository = bookingRepository;
     }
+    public Booking getBookingById(Long id) {
+        return bookingRepository.findById(id).orElse(null);
+    }
 
     public List<Booking> getAllBookings() {
         return bookingRepository.findAll();
+    }
+    public Booking createBooking(Booking book) {
+        return bookingRepository.save(book);
+    }
+
+    public Booking updateBooking(Long id, Booking booking) {
+        Booking existingBooking = bookingRepository.findById(id).orElse(null);
+
+        if (existingBooking != null) {
+            existingBooking.setSubmitdate(booking.getSubmitdate());
+            existingBooking.setCustomerid(booking.getCustomerid());
+            existingBooking.setRoomid(booking.getRoomid());
+            existingBooking.setBedcount(booking.getBedcount());
+            existingBooking.setNightcount(booking.getNightcount());
+            existingBooking.setStartdate(booking.getStartdate());
+            existingBooking.setEnddate(booking.getEnddate());
+            existingBooking.setCost(booking.getCost());
+            return bookingRepository.save(existingBooking);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteBooking(Long id) {
+        bookingRepository.deleteById(id);
     }
 }

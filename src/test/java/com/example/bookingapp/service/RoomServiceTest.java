cea1606e9c4619bbe0e7f3f14698c884f5ddbfe3
stoilsteve-hub <stoil.steve@me.com>
2026-05-12 12:16:47 +0200

    Add unit test for successful getRoomById in RoomServiceTest

--- src/test/java/com/example/bookingapp/service/RoomServiceTest.java
+++ src/test/java/com/example/bookingapp/service/RoomServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
@@ -37,5 +38,17 @@ class RoomServiceTest {
 
         assertEquals(1, rooms.size());
         verify(roomRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getRoomByIdReturnsRoomWhenExists() {
+        Room room = new Room();
+        room.setRoomNumber("101");
+        when(roomRepository.findById(1L)).thenReturn(Optional.of(room));
+
+        Room foundRoom = roomService.getRoomById(1L);
+
+        assertEquals("101", foundRoom.getRoomNumber());
+        verify(roomRepository, times(1)).findById(1L);
     }
 }

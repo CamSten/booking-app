cc3788762d6569bbd44658f4258d8b97c8f7cbf0
Lukas Jona <Lukas.jona@hotmail.com>
2026-08-21 12:12:07 +0200

    Initial Copy to new Application

---
+++ customer-service/src/main/java/com/example/customer_service/controller/CustomerController.java
@@ -0,0 +1,155 @@
+package com.example.customer_service.controller;
+
+import com.example.customer_service.exception.ActiveBookingException;
+import com.example.customer_service.exception.EmailExistsException;
+import com.example.customer_service.model.Customer;
+import com.example.customer_service.service.CustomerService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+
+
+@Controller
+public class CustomerController {
+
+    private final CustomerService customerService;
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customer")
+    public String showCustomerPage(HttpSession session, Model model) {
+        Long customerId = (Long) session.getAttribute("loginCustomerId");
+
+        if(customerId != null){
+            return "redirect:/profile";
+        }
+
+        model.addAttribute("loginCustomer", new Customer());
+        model.addAttribute("signupCustomer", new Customer());
+
+        return "customer";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("loginCustomer") Customer customer, HttpSession session, Model model,
+                        @RequestParam(required = false) Boolean returnToBook,
+                        @RequestParam(required = false) Long roomId) {
+        Optional<Customer> loggedinCustomer = customerService.loginCustomer(customer.getEmail(), customer.getPassword());
+        System.out.println("roomId is: " + roomId);
+        if (loggedinCustomer.isPresent()) {
+            session.setAttribute("loginCustomerId", loggedinCustomer.get().getId());
+            if (Boolean.TRUE.equals(returnToBook) && roomId != null){
+                return "redirect:/book?roomId=" + roomId;
+            }
+            else{
+                return "redirect:/profile";
+            }
+        }
+        model.addAttribute("error", "Invalid username and/or password");
+        model.addAttribute("loginCustomer", new Customer());
+        model.addAttribute("signupCustomer", new Customer());
+
+        return "customer";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute("signupCustomer") Customer customer, HttpSession session, Model model,
+                         @RequestParam(required = false) Boolean returnToBook,
+                         @RequestParam(required = false) Long roomId) {
+        try{Customer createdCustomer = customerService.createCustomer(customer);
+            session.setAttribute("loginCustomerId", createdCustomer.getId());
+            if(Boolean.TRUE.equals(returnToBook) && roomId != null){
+                return "redirect:/book?roomId=" + roomId;
+            }
+            return  "redirect:/customer";
+        } catch(Exception e){
+            model.addAttribute("signupError", e.getMessage());
+            model.addAttribute("loginCustomer", new Customer());
+            model.addAttribute("signupCustomer", customer);
+
+            return "customer";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+
+        return "redirect:/customer";
+    }
+
+
+    @GetMapping("/profile")
+    public String profile(HttpSession session, Model model) {
+        Long customerId = (Long) session.getAttribute("loginCustomerId");
+
+        if (customerId == null) {
+            return "redirect:/customer";
+        }
+
+        Customer customer = customerService.getCustomerById(customerId);
+
+        model.addAttribute("customer", customer);
+
+        return "profile";
+    }
+
+    @GetMapping("/profile/edit")
+    public String editProfile(HttpSession session, Model model) {
+        Long customerId = (Long) session.getAttribute("loginCustomerId");
+        if (customerId == null) {
+            return "redirect:/customer";
+        }
+
+        Customer customer = customerService.getCustomerById(customerId);
+
+        model.addAttribute("customer", customer);
+
+        return "editProfile";
+    }
+
+    @PostMapping("/profile/edit")
+    public String updateProfile(@ModelAttribute("customer") Customer customer, HttpSession session, Model model) {
+        Long customerId = (Long) session.getAttribute("loginCustomerId");
+
+        if (customerId == null) {
+            return "redirect:/customer";
+        }
+
+        try {
+            customerService.updateCustomer(customerId, customer);
+
+        } catch (EmailExistsException e) {
+            model.addAttribute("editError", e.getMessage());
+
+            return "editProfile";
+        }
+
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/profile/delete")
+    public String deleteCustomer(HttpSession session, RedirectAttributes redirectAttributes) {
+        Long customerId = (Long) session.getAttribute("loginCustomerId");
+
+        if (customerId == null) {
+            return "redirect:/customer";
+        }
+        try {
+            customerService.deleteCustomer(customerId);
+            session.invalidate();
+
+            return "redirect:/customer";
+
+        } catch (ActiveBookingException e) {
+            redirectAttributes.addFlashAttribute("deleteError", e.getMessage());
+
+            return "redirect:/profile";
+        }
+    }
+}
---
+++ customer-service/src/main/java/com/example/customer_service/model/Customer.java
@@ -0,0 +1,77 @@
+package com.example.customer_service.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String address;
+    private String phone;
+    private String password;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String email, String address, String phone, String password) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

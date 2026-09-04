ba83c05b7219b34577854fdf904f609b482e4901
stoilsteve-hub <stoil.steve@me.com>
2026-09-04 08:39:19 +0200

    Added @CrossOrigin to ReviewController so frontend can fetch data

--- review-service/src/main/java/com/example/reviewservice/controller/ReviewController.java
+++ review-service/src/main/java/com/example/reviewservice/controller/ReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reviews")
+@CrossOrigin(origins = "*")
 public class ReviewController {
     private final ReviewService reviewService;
 
